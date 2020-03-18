7abb85c259231e5303980dd956f07268dd55efa6
tarquin-the-brave <tomsteavenson@gmail.com>
2020-03-18 19:01:54 +0000

    part 2 of day one - recursion!

--- problem1/app/Main.hs
+++ problem1/app/Main.hs
@@ -7,4 +7,5 @@ main :: IO ()
 main = do
   contents <- readFile "inputs.txt"
   let inputs = fmap read . lines $ contents
-  print . Lib.tot $ inputs
+  print . Lib.tot_1 $ inputs
+  print . Lib.tot_2 $ inputs
--- problem1/src/Lib.hs
+++ problem1/src/Lib.hs
@@ -1,10 +1,22 @@
 module Lib
-    ( f,
-    tot
+    ( tot_1
+    , tot_2
     ) where
 
 f :: Integral a => a -> a
 f x = x `div` 3 - 2
 
-tot :: Integral a => [a] -> a
-tot = sum . fmap f
+tot_1 :: Integral a => [a] -> a
+tot_1 = tot f
+
+g :: Integral a => a -> a
+g x
+  | f x >= 0 = f x + g(f x)
+  | otherwise = 0
+
+tot_2 :: Integral a => [a] -> a
+tot_2 = tot g
+
+tot :: Integral a => (a -> a) -> [a] -> a
+tot f = sum . fmap f
+
