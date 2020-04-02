9cd408492f474d5d344fb734e1e75dfda17e1be3
tarquin-the-brave <tomsteavenson@gmail.com>
2020-04-02 18:28:35 +0100

    notes for part 2

--- day7/app/Main.hs
+++ day7/app/Main.hs
@@ -14,7 +14,7 @@ main = do
   -- contents <- fmap lines . readFile $ "example-3.txt"
   let ampCode = fmap read . head . fmap (splitOn ",") $ contents :: [Int]
 
-  let runAmp = amplifyer ampCode
+  let runAmp = amplifyerPart1 ampCode
 
   let results = fmap (\comb -> (foldM runAmp 0 comb, comb)) phaseCombinations :: [(Maybe Int, [Int])]
 
@@ -24,19 +24,8 @@ main = do
 
   -- PART 2:
 
-amplifyer :: [Int] -> Int -> Int -> Maybe Int
-amplifyer ampCode i phase = case progState finishedProg of
-  Terminated -> if (input finishedProg == []) && ((length . output $ finishedProg) == 1)
-    then Just (head $ output finishedProg)
-    else Nothing
-  _ -> Nothing
-  where finishedProg = runProg . newProg ampCode $ [phase, i]
-
 phaseCombinations :: [[Int]]
 phaseCombinations = permutations [0..4]
-
-runCircuit :: (Int -> Int -> Maybe Int) -> Int -> [Int] -> Maybe Int
-runCircuit f init = foldM f init
 
 maxOut :: [(Maybe Int, [Int])] -> Maybe (Int, [Int])
 maxOut = foldl maxOutFoldFunc (Just (minBound, []))
--- day7/src/Lib.hs
+++ day7/src/Lib.hs
@@ -6,3 +6,25 @@ module Lib
     ) where
 
 import qualified Intcode as Intcode
+
+-- In part 1 we didn't car about keeping the state from running
+-- each amplifyer, so we can take the output of finishedProg
+-- and throw away that state.
+amplifyerPart1 :: [Int] -> Int -> Int -> Maybe Int
+amplifyerPart1 ampCode i phase = case progState finishedProg of
+  Terminated -> if (input finishedProg == []) && ((length . output $ finishedProg) == 1)
+    then Just (head $ output finishedProg)
+    else Nothing
+  _ -> Nothing
+  where finishedProg = runProg . newProg ampCode $ [phase, i]
+
+-- In part 2 amplifiers run in a loop till the programs terminate.
+-- I think this means that the intcode computer will need a new state
+-- of AwaitInput.  As the amplifiers are looped through they will
+-- rest in AwaitInput state until the "final" loop.
+-- Behaviour of runProg will need take account of input program not
+-- being fresh.
+-- Suggest a giant state monad: s -> (a, s)
+-- s = [Amp]
+-- a = Int
+-- data Amp = Amp {ampProgram::Prog, ampPhase::Int}
