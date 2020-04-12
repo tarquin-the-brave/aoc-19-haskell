a7ce2e8bc61458e30801623d58d116904ed0740f
tarquin-the-brave <tomsteavenson@gmail.com>
2020-04-12 23:04:32 +0100

    day 10 answers found

--- day10/README.md
+++ day10/README.md
@@ -1,1 +1,5 @@
 # day10
+
+Possible perf improvements:
+* for part 1, ordering of rays doesn't matter, use hashmaps
+* split up point so D.Coord component can be used for the key of a hashmap, or implement hashable for Point.
--- day10/app/Main.hs
+++ day10/app/Main.hs
@@ -5,7 +5,8 @@ import qualified Display as D
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 import qualified Data.List as L
-import Data.Ratio as R
+import Diagrams.Angle
+import Control.Monad.State.Lazy
 
 main :: IO ()
 main = do
@@ -21,25 +22,29 @@ main = do
 
   print ourMax
 
-data Direction = PosInfinity | NegInfinity | Infront (R.Ratio Int) | Behind (R.Ratio Int) deriving(Show, Eq, Ord)
+  -- PART 2: Spinning lazer
+  let ourPoint = Point { px = fst $ fst ourMax, py = snd $ fst ourMax, pAst = True }
+  let ourRaysWithAsteroids = Map.filter (/=[]) . Map.map (\ps -> [p|p<-ps, pAst p]) . raysFromPoint ourPoint $ Set.delete ourPoint pointSet
+  let raysSorted = sortRayPoints ourPoint ourRaysWithAsteroids
+
+  print $ Map.size raysSorted
+  -- print raysSorted
+  print $ vapourize 200 raysSorted
+
+
 data Point = Point {px::Int, py::Int, pAst::Bool} deriving(Show, Eq, Ord)
-type Rays = Map.Map Direction [Point]
+type Rays = Map.Map (Angle Float) [Point]
 
 getPoints :: [[Char]] -> [Point]
 getPoints = L.intercalate [] . fmap (\(y, row) -> fmap (\(x, b) -> Point{px=x,py=y,pAst=b}) row) . zip [0..] . fmap (zip [0..]) . fmap (fmap (=='#'))
 
 raysFromPoint :: Point -> Set.Set Point -> Rays
-raysFromPoint p0 = foldl (\rays p -> rayInsert (directionFromPoint p0 p) [p] rays) Map.empty
+raysFromPoint p0 = foldl (\rays p -> rayInsert (angleFromPoints p0 p) [p] rays) Map.empty
 
-directionFromPoint :: Point -> Point -> Direction
-directionFromPoint p0 p
-  | px p == px p0 = if (py p) > (py p0) then PosInfinity else NegInfinity
-  | px p < px p0 = Behind ratio
-  | otherwise = Infront ratio
-  where
-    ratio = (((py p) - (py p0)) R.% ((px p) - (px p0)))
+angleFromPoints :: Point -> Point -> Angle Float
+angleFromPoints p0 p = fmap (*(-1)) $ atan2A (fromIntegral ((px p) - (px p0))) (fromIntegral ((py p) - (py p0)))
 
-rayInsert :: Direction -> [Point] -> Rays -> Rays
+rayInsert :: Angle Float -> [Point] -> Rays -> Rays
 rayInsert = Map.insertWith (\p ps -> p ++ ps)
 
 asteroidsSeen :: Rays -> Int
@@ -51,6 +56,39 @@ pointsHaveAst = any id . fmap pAst
 maxSnd :: [(D.Coord , Int)] -> (D.Coord, Int)
 maxSnd = foldl (\acc x -> if snd x > snd acc then x else acc) ((0,0), 0)
 
+sortRayPoints :: Point -> Rays -> Rays
+sortRayPoints p0 = Map.map $ L.sortBy (\p1 p2-> compare (manmod p2) (manmod p1))
+  where
+    manmod p = ((px p) - (px p0)) + ((py p) - (py p0))
+
+shoot' :: Int -> Rays -> ((Point, Int), Rays)
+shoot' count rays = ((asteroid, count + 1), newRays)
+  where
+    idx = (count `mod` (Map.size rays))
+    (angle, ray) = Map.elemAt idx rays
+    asteroid = head ray
+    remainingRay = tail ray
+    newRays = Map.insert angle remainingRay rays
+    -- NOT ROBUST AGAINST FULL ROTATION
+    -- newRays = if remainingRay == []
+    --   then
+    --     Map.delete angle rays
+    --   else
+    --     Map.insert angle remainingRay rays
+
+shoot :: Int -> State Rays (Point, Int)
+shoot = state . shoot'
+
+shootUntil :: Int -> Int -> State Rays Point
+shootUntil imax i = do
+  (point, shot) <- shoot i
+  if shot == imax then
+    return point
+  else
+    shootUntil imax shot
+
+vapourize :: Int -> Rays -> Point
+vapourize n = evalState (shootUntil (n) 0)
 
 -- Got into a right kurfuffle trying to manage verticals as Infinity from Fractional
 -- Implemented my own instead.
