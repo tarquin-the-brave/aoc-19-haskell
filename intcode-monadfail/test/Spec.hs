26f464f24e316704fcfbe6de4fad856754e5c4bc
tarquin-the-brave <tomsteavenson@gmail.com>
2020-04-21 20:10:13 +0100

    add test for id functor law

--- intcode-monadfail/test/Spec.hs
+++ intcode-monadfail/test/Spec.hs
@@ -10,8 +10,30 @@ main = do
   defaultMain $ tests testProg
 
 tests :: [Int] -> TestTree
-tests ic = testGroup "Day 9 mega test"
+tests ic = testGroup "All Tests" [functionalTests ic, testLaws]
+
+functionalTests :: [Int] -> TestTree
+functionalTests ic = testGroup "Tests of intcode computer function"
   [
     testCase "Intcode from day 9 tests all operations work correctly" $
     (fmap IC.output . IC.run $ IC.new ic [1]) @?= IC.End [2171728567]
   ]
+
+testLaws :: TestTree
+-- testLaws = testGroup "Test Functor, Applicative, & MOnad laws" [testF, testA, testM]
+testLaws = testGroup "Test Functor, Applicative, & Monad laws" [testF]
+
+testF :: TestTree
+-- testF = testGroup "Functor Laws" [fLaw1, fLaw2]
+testF = testGroup "Functor Laws" [fLaw1]
+
+fLaw1 :: TestTree
+fLaw1 = testGroup "Law 1: fmap id = id"
+  [
+    testCase "Running varient" $
+    (fmap id $ IC.Running ()) @?= (id $ IC.Running ())
+
+    , testCase "Crashed varient" $
+    (fmap id $ IC.Crashed "woops") @?= ((id $ IC.Crashed "woops")::IC.Prog ())
+  ]
+
